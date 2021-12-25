ea844cd4377eb1f441fc2febff67fef51608a7bc
草鞋没号 <308487730@qq.com>
2021-12-25 17:08:27 +0800

    mode option

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -9,6 +9,12 @@ const { loader } = getLoader();
 module.exports = function (name) {
   return {
     target: `electron-${name}`,
+    /**
+     * @note
+     * mode 为 development 时生成带代码使用 eval 执行
+     * 个人猜测这样做的好处是使得代码错误在运行时抛出，因为 eval(字符串) 能避开语法检测
+     */
+    mode: process.env.NODE_ENV,
     entry: {
       index: path.join(__dirname, `../src/${name}/index.ts`),
     },
